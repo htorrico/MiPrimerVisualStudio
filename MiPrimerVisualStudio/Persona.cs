b1e474f0e6ef4ea0ce4c71439a555f7f82067951
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-09-28 13:39:54 -0500

    Agregar archivos de proyecto.

---
+++ MiPrimerVisualStudio/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerVisualStudio
+{
+    class Persona
+    {
+        //Set Get son los métodos
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+
+       
+        //1. Escribir la palabra prop
+        //2. Presionar tap 2 veces
+    }
+}
---
+++ MiPrimerVisualStudio/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaración de datos
+            string nombre = "Hugo";
+            string apellido;
+            //Asignar variables
+            apellido = "Torrico";
+
+            /*
+            Console.WriteLine(nombre);
+            Console.WriteLine(apellido);
+            */
+
+            //Instanciar los objetos
+            
+            //Primera forma
+            Persona persona = null;
+            persona = new Persona();
+            persona.Nombre = "Hugo";
+            persona.Apellido = "Torrico";
+            persona.Edad = 32;
+
+            //Segunda forma
+            Persona persona1 = new Persona();
+            persona1.Nombre = "Juan";
+            persona1.Apellido = "Torrico";
+            persona1.Edad = 32;
+
+            //Tercera forma
+            Persona persona2 = new Persona { Apellido = "Torrico", Nombre = "Janeth", Edad = 32 };
+            //Console.WriteLine(persona.Nombre);
+            //Console.WriteLine(persona1.Apellido);
+            //Console.WriteLine(persona2.Edad);
+
+
+            //Lista de objetos
+            List<Persona> personas = new List<Persona>();
+            personas.Add(persona);
+            personas.Add(persona1);
+            personas.Add(persona2);
+
+            foreach (var item in personas)
+            {
+                Console.WriteLine(item.Nombre + " "+ item.Apellido);
+            }
+
+
+     
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
